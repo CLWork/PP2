fe4b9c1dd0bb30a054888a24e38c7a5796c714bb
CLWorkman <clworkman@student.fullsail.edu>
2019-02-06 14:44:03 -0500

    Excersie 1 has been created

---
+++ WorkmanCiera_Exercise1/WorkmanCiera_Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ciera Workman
+             *  Project & Portfolio 2
+             *  Exercise 1
+             */
+
+            //Monthly Topic - Feb - Clothing Brands
+        }
+    }
+}
