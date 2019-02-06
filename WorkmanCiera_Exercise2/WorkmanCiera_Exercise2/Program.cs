0459ee158d2600a15e616a297e49c1e796f1929a
CLWorkman <clworkman@student.fullsail.edu>
2019-02-06 14:51:12 -0500

    Exercise 2 has been created.

---
+++ WorkmanCiera_Exercise2/WorkmanCiera_Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ciera Workman
+             * Project & Portfolio 2
+             * Exercise 2
+             */
+
+             //Monthly Topic - Feb - Clothing Brands
+             //Clue game
+        }
+    }
+}
