2b35610ed63b1cc79f9a491d98c0e77d9304cf44
CLWorkman <clworkman@student.fullsail.edu>
2019-02-11 22:34:07 -0500

    Exercise 3 created.

---
+++ WorkmanCiera_Exercise3/WorkmanCiera_Exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ciera Workman
+             * Project & Portfolio 2
+             * Exercise 3 - Calculate Grades
+             */
+        }
+    }
+}
