d81d8eda63f1c071be79378f1ada2ecfe6090b3a
CLWorkman <clworkman@student.fullsail.edu>
2019-02-11 22:34:31 -0500

    Exercise 4 created.

---
+++ WorkmanCiera_Exercise4/WorkmanCiera_Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /* Ciera Workman
+               Project & Portfolio
+               Exercise 4 - Color Dictonary
+            */
+        }
+    }
+}
