000ab8f88d040bce529060fb390255c8f09d18e3
CLWorkman <clworkman@student.fullsail.edu>
2019-02-13 23:58:41 -0500

    Exercise 5 file created.

---
+++ WorkmanCiera_Exercise5/WorkmanCiera_Exercise5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ciera Workman
+             * Project & Portfolio 2
+             * Exercise 5
+             * Rock Paper Scissors Lizard Spock
+             */
+        }
+    }
+}
