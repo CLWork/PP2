edc578029d7a8e30e0c619bffd131ad1a5435343
CLWorkman <clworkman@student.fullsail.edu>
2019-02-14 00:00:31 -0500

    Exercise 6 file created.

---
+++ WorkmanCiera_Exercise6/WorkmanCiera_Exercise6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ciera Workman
+             * Project & Portfolio 2
+             * Card Game
+             */
+        }
+    }
+}
