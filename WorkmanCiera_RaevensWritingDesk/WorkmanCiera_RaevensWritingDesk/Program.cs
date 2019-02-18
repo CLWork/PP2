4264af1d9d5ecf3b7037b672864f60e8ae4b7898
CLWorkman <clworkman@student.fullsail.edu>
2019-02-18 16:43:35 -0500

    Code file created.

---
+++ WorkmanCiera_RaevensWritingDesk/WorkmanCiera_RaevensWritingDesk/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkmanCiera_RaevensWritingDesk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ciera Workman
+             * Project & Portfolio 2
+             * Raeven's Writing Desk Code Files
+             */
+        }
+    }
+}
